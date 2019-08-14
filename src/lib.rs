f7a4aa1942ab917ec5738b6c4d75f5d984ed9b02
ikubaku <hide4d51@gmail.com>
2019-08-14 17:10:45 +0900

    Implement UART

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,26 @@ use core::panic::PanicInfo;
 pub mod gpio;
 use gpio::PortFunction;
 
+pub mod uart;
+
 #[no_mangle]
 pub extern "C" fn main() {
-    // set GPIO17 to OUTPUT
+    // set GPIO17 as Out(LED)
     gpio::set_port_function(17, &PortFunction::Output);
+    gpio::set_port(17);
+    // set GPIO14 as Alt0(TXD0)
+    gpio::set_port_function(14, &PortFunction::Alt0);
+    // set GPIO15 as Alt0(RXD0)
+    gpio::set_port_function(15, &PortFunction::Alt0);
+
+    uart::init();
+    uart::set_baudrate(115200);
 
     loop {
-        // set GPIO17 to HIGH
-        gpio::set_port(17);
-
-        // busy wait
-        for _ in 1..10000000 {
-            unsafe {
-                asm!("");
-            }
+        for c in "Hello, world!\r\n".as_bytes() {
+            uart::write_data(*c);
         }
-
-        // set GPIO17 to LOW
-        gpio::reset_port(17);
-
-        // busy wait
-        for _ in 1..10000000 {
+        for _i in 0..1000000 {
             unsafe {
                 asm!("");
             }
---
+++ src/uart.rs
@@ -0,0 +1,135 @@
+//#![no_std]
+use core::intrinsics::{volatile_store, volatile_load};
+
+const PERIF_BASE_ADDR: u32 = 0x3F000000;
+const UART_BASE_ADDR: u32 = PERIF_BASE_ADDR + 0x00201000;
+const UART_FREQ: u32 = 48_000_000;
+
+fn enable() {
+    let uart_cr = (UART_BASE_ADDR + 0x30) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_cr, *(uart_cr) | 0x00000001);
+    }
+}
+
+fn disable() {
+    let uart_cr = (UART_BASE_ADDR + 0x30) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_cr, *(uart_cr) & !0x00000001);
+    }
+}
+
+fn wait_for_transmission() {
+    loop {
+        let uart_fr = unsafe { volatile_load((UART_BASE_ADDR + 0x18) as *mut u32) };
+        if uart_fr & 0x00000008 == 0 {
+            break;
+        }
+    }
+}
+
+fn wait_for_data() {
+    loop {
+        let uart_fr = unsafe { volatile_load((UART_BASE_ADDR + 0x18) as *mut u32) };
+        if uart_fr & 0x00000040 == 0 {
+            break;
+        }
+    }
+}
+
+fn wait_for_peer() {
+    loop {
+        let uart_fr = unsafe { volatile_load((UART_BASE_ADDR + 0x18) as *mut u32) };
+        if uart_fr & 0x00000020 == 0 {
+            break;
+        }
+    }
+}
+
+fn disable_fifo() {
+    let uart_lcrh = (UART_BASE_ADDR + 0x2C) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_lcrh, *(uart_lcrh) & !0x00000010);
+    }
+}
+
+fn enable_fifo() {
+    let uart_lcrh = (UART_BASE_ADDR + 0x2C) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_lcrh, *(uart_lcrh) | 0x00000010);
+    }
+}
+
+fn get_divisor(rate: u32) -> (u32, u32) {
+    let bdiv_i = UART_FREQ / (16 * rate);
+    let temp = (1000 * UART_FREQ as u64 / (16 * rate as u64) - 1000 * bdiv_i as u64) * 64 + 500;
+    let bdiv_f= (temp / 1000) as u32;
+
+    return (bdiv_i, bdiv_f);
+}
+
+pub fn init() {
+    disable();
+    wait_for_transmission();
+    disable_fifo();
+
+    // word size=8bit, with start/stop bit, no parity
+    let uart_lcrh = (UART_BASE_ADDR + 0x2C) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_lcrh, *(uart_lcrh) | 0x00000070);
+    }
+
+    // enable TX and RX, disable flow control
+    let uart_cr = (UART_BASE_ADDR + 0x30) as *mut u32;
+
+    unsafe {
+        volatile_store(uart_cr, 0x00000300);
+    }
+
+    enable();
+}
+
+pub fn set_baudrate(rate: u32) {
+    // stop transmission before changing UART_CR register
+    disable();
+    wait_for_transmission();
+    // flush fifo
+    disable_fifo();
+
+    // set baudrate divisor
+    let (bdiv_i, bdiv_f) = get_divisor(rate);
+    let uart_ibrd = (UART_BASE_ADDR + 0x24) as *mut u32;
+    let uart_fbrd = (UART_BASE_ADDR + 0x28) as *mut u32;
+    unsafe {
+        volatile_store(uart_ibrd, bdiv_i);
+        volatile_store(uart_fbrd, bdiv_f);
+    }
+
+    // enable fifo
+    enable_fifo();
+    // enable UART
+    enable();
+}
+
+pub fn write_data(data: u8) {
+    let uart_dr_data = (UART_BASE_ADDR + 0x00) as *mut u8;
+
+    wait_for_peer();
+
+    unsafe {
+        volatile_store(uart_dr_data, data);
+    }
+}
+
+pub fn read_data() -> u8 {
+    let uart_dr_data = (UART_BASE_ADDR + 0x00) as *mut u8;
+
+    wait_for_data();
+
+    return unsafe { volatile_load(uart_dr_data) }
+}
