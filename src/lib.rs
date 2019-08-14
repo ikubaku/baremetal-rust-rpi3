f7c354c2379a0174aef4728f7adf4b30d7b93df2
ikubaku <hide4d51@gmail.com>
2019-08-14 09:28:36 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,31 @@
 #![feature(lang_items, asm, core_intrinsics)]
 #![no_std]
-use core::intrinsics::volatile_store;
 use core::panic::PanicInfo;
+
+pub mod gpio;
+use gpio::PortFunction;
 
 #[no_mangle]
 pub extern "C" fn main() {
-    // GPIOSEL1
-    let gpfsel1 = 0x3F200004 as *mut u32;
-    // GPSET0
-    let gpset0 = 0x3F20001C as *mut u32;
-    // GPCLR0
-    let gpclr0 = 0x3F200028 as *mut u32;
-
     // set GPIO17 to OUTPUT
-    unsafe {
-        volatile_store(gpfsel1, *(gpfsel1) & !((0x07 as u32) << (7 * 3 as u32)));
-        volatile_store(gpfsel1, *(gpfsel1) | ((0x01 as u32) << (7 * 3 as u32)));
-    }
+    gpio::set_port_function(17, &PortFunction::Output);
 
     loop {
-        // GPSET0: set GPIO17 to HIGH
-        unsafe {
-            volatile_store(gpset0, 1 << 17 as u32);
-        }
+        // set GPIO17 to HIGH
+        gpio::set_port(17);
 
         // busy wait
-        for _ in 1..1000000 {
+        for _ in 1..10000000 {
             unsafe {
                 asm!("");
             }
         }
 
-        // GPCLR0: set GPIO17 to LOW
-        unsafe {
-            volatile_store(gpclr0, 1 << 17 as u32);
-        }
+        // set GPIO17 to LOW
+        gpio::reset_port(17);
 
         // busy wait
-        for _ in 1..1000000 {
+        for _ in 1..10000000 {
             unsafe {
                 asm!("");
             }
